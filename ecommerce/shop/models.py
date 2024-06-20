7c4c18fc526fd529790cbadd0a821ec652630272
Elsa <elsa.rousselot@hotmail.fr>
2024-06-20 15:25:24 +0200

    création class formule, changement class evenement et class catégorie

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,28 +1,35 @@
 from django.db import models
 
 #création table
-class Category(models.Model):
+class Discipline (models.Model):
     name = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now=True)
     
-#faire que quand on ajoute un produit c'est en premier 
-
+#faire que quand on ajoute une discipline elle se met en premier dans la liste
     class Meta:
         ordering = ['-date_added']
     def __str__(self):
         return self.name
     
     
-#création table des produits
-
-class Product(models.Model):
+#création table des evenements 
+class Evenement (models.Model):
     title = models.CharField(max_length=200)
-    price = models.FloatField()
     description = models.TextField()
-    category = models.ForeignKey(Category, related_name = 'categorie', on_delete=models.CASCADE)
-    #image = models.CharField(max_length=5000, )
+    category = models.ForeignKey(Discipline, related_name = 'Discipline', on_delete=models.CASCADE)
+    date_event = models.DateTimeField()
+    image = models.CharField(max_length=5000, )
     date_added = models.DateTimeField(auto_now=True)
     class Meta:
         ordering = ['-date_added']
     def __str__(self):
         return self.title
+    
+    # faire la création d'une classe formule avec prix nombre de personne titre et une foreign key vers evenement 
+    
+class Formule (models.Model):
+    formule = models.CharField(max_length=200)
+    price = models.FloatField()
+    evet_rattache = models.ForeignKey(Evenement, related_name = 'EV', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.formule
