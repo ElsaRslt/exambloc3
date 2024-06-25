7b8d4f72fd136effe9aa0169fd3e103c547ff5b6
Elsa <elsa.rousselot@hotmail.fr>
2024-06-25 08:58:26 +0200

    création de la branche pour afficher les evenements sur la page d'acceuil

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Evenement
 
 
-#fonction qui va permettre d'afficher le fichier index 
+#fonction qui va permettre d'afficher le fichier index et les images
 def index(request):
-    return render(request, 'index.html')
+    product_object =Evenement.objects.all() #selection de tous les evenements qui sont dans la BDD
+    return render(request, 'index.html', {'product_object': product_object})
