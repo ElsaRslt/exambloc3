7fbea07bdd3652147b4e3e3100e2155fe784d3a3
Elsa <elsa.rousselot@hotmail.fr>
2024-06-27 11:00:49 +0200

    affichage du prix selon la formule choisie

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -10,6 +10,15 @@ class Discipline (models.Model):
     def __str__(self):
         return self.name
     
+
+# Création d'une classe formule avec prix nombre de personne titre et une foreign key vers evenement 
+class Formule (models.Model):
+    formule = models.CharField(max_length=200)
+    price_multiplier = models.FloatField()
+    # evet_rattache = models.ForeignKey(Evenement, related_name = 'EV', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.formule
+
     
 #création table des evenements 
 class Evenement (models.Model):
@@ -17,9 +26,10 @@ class Evenement (models.Model):
     description = models.TextField()
     category = models.ForeignKey(Discipline, related_name = 'Discipline', on_delete=models.CASCADE)
     date_event = models.DateTimeField()
-    #image = models.CharField(max_length=5000, )
     image = models.ImageField(upload_to='event_images/')
     date_added = models.DateTimeField(auto_now=True)
+    formules = models.ManyToManyField(Formule, related_name ='formule_choisie')
+    base_price = models.FloatField(default=10.0)
     
     #le dernier evenement créé se place en premier dans la liste 
     class Meta:
@@ -28,10 +38,3 @@ class Evenement (models.Model):
         return self.title
     
 
-# Création d'une classe formule avec prix nombre de personne titre et une foreign key vers evenement 
-class Formule (models.Model):
-    formule = models.CharField(max_length=200)
-    price = models.FloatField()
-    evet_rattache = models.ForeignKey(Evenement, related_name = 'EV', on_delete=models.CASCADE)
-    def __str__(self):
-        return self.formule
