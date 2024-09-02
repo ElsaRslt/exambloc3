9f13f7bc557a812f8a1736cf2cf70ac867904cb6
Elsa <elsa.rousselot@hotmail.fr>
2024-09-02 13:57:00 +0200

    finalisation page panier

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Evenement, Formule
 from django.db.models import Q  # Import de l'opérateur Q pour les requêtes complexes
 from django.core.paginator import Paginator
