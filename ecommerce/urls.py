38f1d12a2a7ec477fdcd221cdbcafe74de6c374d
Elsa <elsa.rousselot@hotmail.fr>
2024-10-16 14:21:18 +0200

    feat(footer) : ajouter le lien admin dans le footer

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,9 +6,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
+
 urlpatterns = [
     
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     
     path('', views.index, name ='home'), 
     
